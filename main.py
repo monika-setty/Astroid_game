b1c9dab1a1db42c1d2b876fc69f25e29d4f76960
monika_Vummadi_Git <monika.setty@toyota.com>
2025-07-22 21:03:40 -0500

    adding the whole code

--- main.py
+++ main.py
@@ -1,20 +1,59 @@
+#!/usr/bin/env python3
 # this allows us to use cde from
 # the open-source pygame library
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+import sys
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clk= pygame.time.Clock()
+    dt=0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    asteroids=pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers=(updatable)
+    af=AsteroidField()
+    shots=pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    # Monika=Player(x,y)
     while(True):
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 return
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                    if asteroid.collides_with(shot):
+                        shot.kill()
+                        asteroid.split()
+
+                
+
         screen.fill((0,0,0))
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+        time_paused=clk.tick(60)
+        dt=(time_paused)/1000
 
 if __name__ == "__main__":
     main()
